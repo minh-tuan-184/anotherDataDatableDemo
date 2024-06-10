e11ddd58314b67b41f0aafb756938c94ede6a15d
minh-tuan-184 <81298170+minh-tuan-184@users.noreply.github.com>
2024-06-10 15:20:05 +0700

    fix bug when commit

---
+++ common/src/main/java/org/sakaiproject/tool/mydatatable/common/TableStructure.java
@@ -0,0 +1,18 @@
+package org.sakaiproject.tool.mydatatable.common;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TableStructure {
+	int[] colWidths;
+	String []colHeaders;
+	List<Object[]> data;
+}
+
